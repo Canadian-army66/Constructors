2cddde82e6f0bf033cde2e0b8d32aed9f50f76d0
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-04-24 08:59:52 -0500

    done do the project

---
+++ Constructors_Kohnen_Christian/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_Kohnen_Christian
+{
+    class Game //creates the class of Game
+    {
+        public string title, genre; //vreates string variables
+        public Game(string titleParam = "Unknown Title", string genreParam = "Casual") //makes default construstor to Unknown title and Casual
+        {
+            title = titleParam; //assigns default param to title
+            genre = genreParam; //assigns default oparam to genre
+        }
+    }
+}
--- Constructors_Kohnen_Christian/Program.cs
+++ Constructors_Kohnen_Christian/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Game myGame = new Game("Lurid", "Physological Horror"); //assigns new value to title and genre
+            Console.WriteLine($"My favorite game is {myGame.title} and it is a {myGame.genre} game!"); //prints with updated values
+
+            Game myOtherGame = new Game(); //does not assign a value to title and genre
+            Console.WriteLine($"My favorite game is {myOtherGame.title} and it is a {myOtherGame.genre} game!"); //It prints the default value stated in Class.cs
         }
     }
 }
